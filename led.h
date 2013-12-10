baa89ce5e55a7575fb0cd9d63de408348491155a
Shaun Evans <evans.shaunmichael@gmail.com>
2013-12-09 22:40:57 -0700

    Starting to put it together

---
+++ led.h
@@ -0,0 +1,16 @@
+#include <wiringPi.h>
+#include <wiringSerial.h>
+#include <iostream>
+#include <sstream>
+
+using namespace std;
+
+class serialLed()
+{
+	//bool isOn;
+public:
+//connect takes a char as the com port and an int as the baud rate returns true if the com is open
+	bool connect( char com, int baud);
+//toggle switches the led to a different state
+	void state(char onOff);
+};
